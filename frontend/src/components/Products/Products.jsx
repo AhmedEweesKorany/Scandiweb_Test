9f14fda10b2bb1a0f9e5ac185380d6175bb77a67
AhmedEweesKorany <ahmedewaisvi34@gmail.com>
2024-10-07 10:36:36 +0300

    quick add to cart feature

--- frontend/src/components/Products/Products.jsx
+++ frontend/src/components/Products/Products.jsx
@@ -5,6 +5,7 @@ import logo from "../../assets/logo.png";
 import NavContext from '../../Context/NavContext';
 import { Link } from 'react-router-dom';
 import { IoCartOutline } from "react-icons/io5";
+import CartContext from '../../Context/CartContext';
 
 
 class Products extends Component {
@@ -33,6 +34,13 @@ class Products extends Component {
                                 amount
                                 currency
                             }
+                                 attributes{
+      id
+      items
+      type
+      name
+    }
+                            
                         }
                     }
                 `,
@@ -43,10 +51,10 @@ class Products extends Component {
             .catch((error) => console.error(error)); 
     }
 
-    renderProduct(product, index) {
+    renderProduct(product, index,cartData,setCartData) {
         const galleryImage = JSON.parse(product.gallery) ? JSON.parse(product.gallery) : logo;
         const price = JSON.parse(product.prices[0].currency).symbol + product.prices[0].amount;
-        console.log(product)
+        console.log(product?.attributes )
         return (
             <div key={index} className='hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-3 cursor-pointer group mb-10'>
              <Link to={`/product/${product.id}`}>
@@ -56,7 +64,7 @@ class Products extends Component {
                 </div>
              </Link>
 
-             {product.inStock && <div className='hidden group-hover:flex  w-[50px] h-[50px] bg-green-500 text-white rounded-full float-end items-center justify-center' onClick={()=>console.log("cart")}>
+             {product.inStock && <div className='hidden group-hover:flex  w-[50px] h-[50px] bg-green-500 text-white rounded-full float-end items-center justify-center' onClick={()=>this.handleQuickAdd(cartData,setCartData,product)}>
                 <IoCartOutline/>
                  </div>}
             
@@ -68,21 +76,54 @@ class Products extends Component {
         );
     }
 
+    handleQuickAdd = (cartData,setCartData,product)=>{
+       
+        setCartData([...cartData,{
+            id:product.id,
+            name:product.name,
+            price:product.prices[0].amount,
+            currency:JSON.parse(product.prices[0].currency).symbol,
+            attributes:product.attributes,
+            selectedAttr:product?.attributes.length > 0 ? this.defaultAttr(product?.attributes) : [],
+            quantity:1,
+            gallery:product.gallery
+          }])
+    }
+
+    defaultAttr = (attributes)=>{
+        let res = {}
+        attributes.map((attr)=>{
+            JSON.parse(attr.items).map((pr)=>{
+                if(res[attr.id] === undefined){
+
+                    res[attr.id] = pr.value
+                }
+            })
+        })
+
+        return res
+    }
+
     render() {
         const { products } = this.state;
         const cur = this.context[0]; 
 
         return (
+            <CartContext.Consumer>
+                {([cartData,setCartData])=>(
             <div>
+                
                 <h1 className='text-5xl font-bold my-20 uppercase'>{cur}</h1>
                 <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 place-content-center'>
                     {
                         cur === "all" ?
-                            products.map((product, index) => this.renderProduct(product, index)) :
-                            products.filter(product => product.category === cur).map((product, index) => this.renderProduct(product, index))
+                        products.map((product, index) => this.renderProduct(product, index,cartData,setCartData)) :
+                        products.filter(product => product.category === cur).map((product, index) => this.renderProduct(product, index,cartData,setCartData))
                     }
                 </div>
             </div>
+                )}
+                    </CartContext.Consumer>
         );
     }
 }
